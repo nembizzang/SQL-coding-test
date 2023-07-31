ac4b158d07dc6b91b2d2440b122570d8d1ed84d6
nembizzang <105214855+nembizzang@users.noreply.github.com>
2023-08-01 05:29:56 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,6 +1,7 @@
-SELECT a.animal_id, a.animal_type, a.name
-FROM animal_ins a
-JOIN animal_outs o
-ON a.animal_id=o.animal_id AND a.animal_type=o.animal_type AND a.name=o.name
-WHERE sex_upon_intake LIKE '%Intact%' AND sex_upon_outcome NOT LIKE '%Intact%'
-ORDER BY animal_id
+SELECT ins.animal_id, ins.animal_type, ins.name
+    FROM animal_ins ins
+    LEFT OUTER JOIN animal_outs outs
+                ON ins.animal_id = outs.animal_id
+    WHERE (ins.sex_upon_intake LIKE '%Intact%')
+        AND (outs.sex_upon_outcome LIKE '%Spayed%' OR outs.sex_upon_outcome LIKE '%Neutered%')
+    ORDER BY ins.animal_id;
