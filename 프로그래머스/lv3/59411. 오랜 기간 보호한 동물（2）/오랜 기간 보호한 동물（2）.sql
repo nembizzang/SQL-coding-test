86e8a0c9da8e7631f9bc62f418def3a85df58260
nembizzang <105214855+nembizzang@users.noreply.github.com>
2023-08-01 05:18:30 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,5 +1,6 @@
-SELECT i.animal_id, i.name
-FROM animal_ins i, animal_outs o
-WHERE i.animal_id = o.animal_id
-ORDER BY o.datetime - i.datetime DESC
-LIMIT 2
+SELECT ins.animal_id, ins.name
+    FROM animal_ins ins
+    LEFT OUTER JOIN animal_outs outs
+                ON ins.animal_id = outs.animal_id
+    ORDER BY outs.datetime - ins.datetime DESC
+    LIMIT 2;
