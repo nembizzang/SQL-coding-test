3e259632eec2f055d9317809c314eb8e5560fb56
nembizzang <105214855+nembizzang@users.noreply.github.com>
2023-08-04 22:37:12 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,3 +1,4 @@
+# Subquery - inner view 활용
 SELECT info.food_type, info.rest_id, info.rest_name, info.favorites
     FROM rest_info info
     INNER JOIN (SELECT food_type, MAX(favorites) favorites
@@ -5,8 +6,10 @@ SELECT info.food_type, info.rest_id, info.rest_name, info.favorites
                             GROUP BY food_type) fav
                 ON info.favorites = fav.favorites AND info.food_type=fav.food_type
     ORDER BY info.food_type DESC;
-# SELECT food_type, rest_id, rest_name, favorites
-# FROM rest_info
-# WHERE favorites IN (SELECT MAX(favorites) FROM rest_info GROUP BY food_type)
-# GROUP BY food_type
-# ORDER BY food_type DESC
+    
+# Subquery - scalar 활용
+SELECT food_type, rest_id, rest_name, favorites
+FROM rest_info
+WHERE favorites IN (SELECT MAX(favorites) FROM rest_info GROUP BY food_type)
+GROUP BY food_type
+ORDER BY food_type DESC
