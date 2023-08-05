84bd31ce929a615841a8e4bbc135682605078957
nembizzang <105214855+nembizzang@users.noreply.github.com>
2023-08-06 02:22:03 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,4 +1,7 @@
 SELECT *
-FROM places
-WHERE host_id IN (SELECT host_id FROM places GROUP BY host_id HAVING COUNT(host_id) >= 2)
-ORDER BY id
+    FROM places
+    WHERE host_id IN (SELECT host_id
+                    FROM places
+                    GROUP BY host_id
+                    HAVING COUNT(*)>=2)
+    ORDER BY id;
