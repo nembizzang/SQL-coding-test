c0513be415a74d1f4280e47e604d2f57223afbc1
nembizzang <105214855+nembizzang@users.noreply.github.com>
2023-08-11 23:51:38 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,5 +1,6 @@
 # set 함수 이용
-SET @HOUR := -1; # 초기 변수 설정
+SET @HOUR := -1; # 초기 변수 설정, 세미콜론으로 종료 해줘야함
+
 SELECT (@HOUR := @HOUR+1) HOUR, # HOUR 변수에 +1 반복
         (SELECT COUNT(*)
             FROM animal_outs
