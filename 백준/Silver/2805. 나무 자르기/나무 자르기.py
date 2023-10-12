466aafecdb3a4e20fbb037bb6fc480acbc9a3aa7
김영빈 <105214855+nembizzang@users.noreply.github.com>
2023-10-13 03:37:07 +0900

    [Silver II] Title: 나무 자르기, Time: 424 ms, Memory: 115064 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -10,16 +10,15 @@ input = open(0).readline
 def solution(m,trees):
     counter = Counter(trees)
     left,right = 0, 1000000000 # 이분 탐색의 시작과 끝
-    while left<(left+right)//2 :
+    while left<=right :
         mid = (left+right)//2 # 중간
-        tmp_m = 0 # 총 가져가는 나무 길이
-        for tree in counter : # 나무 높이 하나씩을 꺼내서
-            tmp_m += (tree-mid)*counter[tree] if tree>mid else 0
+        # 나무 높이 하나씩을 꺼내서 자른 길이를 더해준다.
+        tmp_m = sum([(key-mid)*value for key,value in counter.items() if key>mid])
         if tmp_m >= m : # 나무 길이가 m이상일때
-            left = mid
+            left = mid+1
         else : # 나무 길이가 m미만일때
-            right = mid
-    return left # while 문을 빠져나오는 순간이 나무 길이가 m이상인 지점이므로
+            right = mid-1
+    return right # while 문을 빠져나오는 순간이 나무 길이가 m이상인 지점이므로
 
 if __name__ == '__main__':
     n,m = map(int,input().split())
