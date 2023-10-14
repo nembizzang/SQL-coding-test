d6ea29b43625834990e54ea174b5933c2b5a7453
김영빈 <105214855+nembizzang@users.noreply.github.com>
2023-10-15 01:52:44 +0900

    [Gold IV] Title: 최단경로, Time: 936 ms, Memory: 73976 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -20,6 +20,10 @@ def solution(sta): # 시작 노드로부터 다익스트라 bfs 진행
         cur_dist,cur_node = heappop(queue) # queue에서 현재 최단 경로와 확인할 노드 뽑기
         if short_cut[cur_node] < cur_dist : # cur_node의 현재 최단 경로가 현재의 경로보다 짧다면
             continue # 확인할 필요없이 다음 노드 확인 진행
+            # 위 경우는 다음과 같은 경우에 작동한다.
+            # 맨 처음에 1->2 : 비용 10인 간선을 확인하고 큐에 넣는다.
+            # 직후에 1->3 : 4 / 3->2 : 2인 간선을 확인했다면 2까지의 최소비용은 6으로 들어가있다.
+            # 나중에 비용 10, 출발 노드 2를 큐에서 뽑는다면 이미 이 상태에서는 최소비용보다 초과해있기에 아래 for문을 안해도 된다.
         for new_node,dist in path[cur_node] : # 현재 노드에서 간선으로 이어진 노드와 경로
             if short_cut[new_node] > cur_dist+dist : # new_node의 현재 최단 경로가 cur_node를 거쳐간 경로 보다 크다면
                 short_cut[new_node] = cur_dist+dist # 최단 경로 갱신
@@ -34,7 +38,7 @@ if __name__ == '__main__':
         sta,end,w = map(int,input().split())
         path[sta].append([end,w])
     inf = float('inf')
-    short_cut = {i:inf for i in range(1,v+1)} # 각 노드까지 최단경로 저장할 dictionary
+    short_cut = [inf]*(v+1) # 각 노드까지 최단경로 저장할 list
     solution(k) # 시작점부터 bfs 시작
     for i in range(1,v+1):
         print(short_cut[i] if short_cut[i] != float('inf') else 'INF')
