9cac8db621439f5b065f5a8e0508fc1a3bc8b6b6
김영빈 <105214855+nembizzang@users.noreply.github.com>
2023-10-22 06:55:21 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,11 +1,12 @@
 SET @hour := -1;
-SELECT @hour := @hour+1 hour,
-    (SELECT COUNT(*)
+SELECT @hour := @hour+1,
+    (SELECT COUNT(animal_id)
         FROM animal_outs
         WHERE HOUR(datetime) = @hour)
     FROM animal_outs
-    WHERE @hour < 23
-    ORDER BY hour;
+    WHERE @hour < 23;
+
+
 
 
 
@@ -25,7 +26,6 @@ SELECT @hour := @hour+1 hour,
 
 # # set 함수 이용
 # SET @HOUR := -1; # 초기 변수 설정, 세미콜론으로 종료 해줘야함
-
 # SELECT (@HOUR := @HOUR+1) HOUR, # HOUR 변수에 +1 반복
 #         (SELECT COUNT(*)
 #             FROM animal_outs
@@ -34,7 +34,7 @@ SELECT @hour := @hour+1 hour,
 #     WHERE @HOUR < 23 # 마지막으로 +1한 HOUR가 23이면 반복 멈춤
 #     ORDER BY HOUR;
 
-# SET @a := 0;
-# SELECT @a := @a+1
-#     FROM animal_outs
-#     WHERE @a < 4;
+# # SET @a := 0;
+# # SELECT @a := @a+1
+# #     FROM animal_outs
+# #     WHERE @a < 4;
